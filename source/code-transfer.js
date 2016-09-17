e6bddadadde5ec7748c6555404ff14ebbeb73000
KrashLeviathan <nathankarasch@gmail.com>
2016-09-17 14:00:07 -0500

    Fixed code in code-transfer.js and wrote a couple tests.

--- source/code-transfer.js
+++ source/code-transfer.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
+var urlPrefix = "https://gforge.com/git/";
+
 
 function transferCode(ghRepo, ghUsername, gfRepo, gfUsername, verbose) {
     // Check if user has git installed
@@ -19,43 +21,47 @@ function transferCode(ghRepo, ghUsername, gfRepo, gfUsername, verbose) {
     shell.exec("git pull --all");
 
     // Setup gforge remote
-    setupGforgeRemote(gforgeRepoURL);
+    setupGforgeRemote(ghRepo);
 
     // Push to new remote
-    shell.exec("git push " + gforgeRepoURL + " --all");
-    shell.exec("git push " + gforgeRepoURL + " --tags");
+    shell.exec("git push " + ghRepo + " --all");
+    shell.exec("git push " + ghRepo + " --tags");
+}
 
 
-    function checkGitInstalled() {
-        if (!shell.which("git")) {
-            shell.echo("Sorry, this script requires git.");
-            shell.exit(1);
-        }
+function checkGitInstalled() {
+    if (!shell.which("git")) {
+        shell.echo("Sorry, this script requires git.");
+        shell.exit(1);
     }
+}
 
-    function attemptToCheckoutMaster() {
-        var commandStdOut;
-        var commandStdErr;
-        shell.exec("git checkout master", function(_, stdout, stderr) {
-            commandStdOut = stdout;
-            commandStdErr = stderr;
-        });
-        if (commandStdErr) {
-            console.log(commandStdErr);
-            shell.exit(1);
-        }
+function attemptToCheckoutMaster() {
+    var commandStdOut;
+    var commandStdErr;
+    shell.exec("git checkout master", function(_, stdout, stderr) {
+        commandStdOut = stdout;
+        commandStdErr = stderr;
+    });
+    if (commandStdErr) {
+        console.log(commandStdErr);
+        shell.exit(1);
     }
+}
 
-    function setupGforgeRemote() {
-        var remotes = shell.exec("git remote -v").stdout;
-        if (remotes.includes("gforge)")) {
-            shell.exec("git remote remove gforge");
-        }
-        shell.exec("git remote add gforge " + gforgeRepoURL);
+function setupGforgeRemote(ghRepo) {
+    var url = urlPrefix + ghRepo;
+    var remotes = shell.exec("git remote -v").stdout;
+    if (remotes.includes("gforge)")) {
+        shell.exec("git remote remove gforge");
     }
+    shell.exec("git remote add gforge " + url);
 }
 
 
 module.exports = {
-    transferCode
+    transferCode,
+    checkGitInstalled,
+    attemptToCheckoutMaster,
+    setupGforgeRemote
 };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
 var assert = require('assert');
+var shell = require('shelljs');
+var ct = require('../source/code-transfer');
 
 
 describe('Array', function() {
@@ -14,17 +16,35 @@ describe('Array', function() {
 
 
 describe('code-transfer', function() {
+    var hook;
+
+    beforeEach(function() {
+        hook = captureStream(process.stdout);
+    });
+
+    afterEach(function() {
+        hook.unhook();
+    });
+
     describe.skip('transferCode(ghRepo, ghUsername, gfRepo, gfUsername, verbose)', function() {
         // TODO
     });
 
-    describe.skip('checkGitInstalled()', function() {
-        // TODO
+    describe('checkGitInstalled()', function() {
         it('should not display an error message if git is installed', function() {
-            // ...
+            ct.checkGitInstalled();
+            assert.equal(hook.captured(), '');
         });
-        it('should return exit code 1 and display error if git isn\'t installed', function() {
-            // ...
+        it('should return exit code 1 and display error if (mock) git isn\'t installed', function() {
+            // Won't really test functionality if the code changes.
+            // Mostly just to test that the logic is sound and works.
+            function mockCheckGitInstalled() {
+                if (!shell.which("gitnotinstalled")) {
+                    shell.echo("Sorry, this script requires git.");
+                }
+            }
+            mockCheckGitInstalled();
+            assert.equal(hook.captured(), 'Sorry, this script requires git.\n');
         });
     });
 
@@ -47,3 +67,23 @@ describe('code-transfer', function() {
 describe.skip('issues-transfer', function() {
     // TODO
 });
+
+
+// This utility is used for capturing stdout for testing
+function captureStream(stream){
+    var oldWrite = stream.write;
+    var buf = '';
+    stream.write = function(chunk, encoding, callback) {
+        buf += chunk.toString(); // chunk is a String or Buffer
+        oldWrite.apply(stream, arguments);
+    };
+
+    return {
+        unhook: function unhook() {
+            // stream.write = oldWrite;
+        },
+        captured: function() {
+            return buf;
+        }
+    };
+}
