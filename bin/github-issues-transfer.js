25a762898f86b802b5afc7e951d196cf33eb5757
Justin Wheeler <Justin Wheeler>
2016-09-17 03:38:42 -0500

    temp merge

--- bin/github-issues-transfer.js
+++ bin/github-issues-transfer.js
@@ -1,3 +1,80 @@
-#! /usr/bin/env node
 
-console.log("Transferring Issues from GitHub to GForge...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const https = require('https');
+var gitURL = {
+  host: 'api.github.com',
+  path: '/',
+  method: 'GET',
+  headers: {'user-agent': 'node.js'}
+};
+
+https.get(url, function(res) {
+  res.on("data", function(chunk) {
+    console.log("BODY: " + chunk);
+  });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+
+
+
+var gForce = {
+  host: 'next.gforge.com',
+  path: '/api/trackeritem/',
+  method: 'POST',
+  headers: {'user-agent': 'node.js'}
+}
+
+var req = http.request(gForce, function(res) {
+  gForce["path"] = '/api/trackeritem/' + object["userID"];
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.write('data\n');
+req.write('data\n');
+req.end();
