9a8325e6b6d82ba77860e1ea10ef70694fec8e24
Gregory Steenhagen <gregory@Gregorys-iMac.local>
2016-09-17 03:34:55 -0500

    Got http get request working

--- bin/github-issues-transfer.js
+++ bin/github-issues-transfer.js
@@ -1,14 +1,44 @@
 #! /usr/bin/env node
-var github = require('octonode');
+const https = require('https');
+
+function transfer_issues(github_username, github_password, github_repo, gforge_username, gforge_password, gforge_repo){
+    var github_issues_json = get_github_issues(github_username, github_repo);
+
+    post_gforge_tracker(json, gforge_username, gforge_password, gforge_repo);
+
+
+}
+
 
 // , github_password, github_repo, gforge_username, gforge_password, gforge_repo
-function transfer_issues(github_username, github_repo){
-    var client = github.client();
+function get_github_issues(github_username, github_repo){
 
-    client.get('/repos/csteamengine/MySnippets/issues', {}, function (err, status, body, headers) {
-        body.forEach(function(curr){
-            console.log(curr['title']);
+
+    var url = {
+        host: 'api.github.com',
+        path: '/repos/'+github_username+'/'+ github_repo +'/issues',
+        method: 'GET',
+        headers: {'user-agent': github_username}
+    };
+
+    https.get(url, function(res) {
+
+        console.log("Got response: " + res.statusCode);
+        var body = "";
+        res.on("data", function(chunk) {
+            body += chunk;
         });
-        // console.log(body); //json object
+        res.on('end', function(){
+            var json = JSON.parse(body);
+            console.log(json[0]);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
     });
 }
+
+function post_gforge_trackers(json, username, password, repo){
+    //TODO
+}
+
+get_github_issues('csteamengine', 'MySnippets');
--- index.js
+++
@@ -1,3 +0,0 @@
-var issues = require('/bin/github-issues-transfer');
-
-transfer_issues('csteamengine','MySnippets');
