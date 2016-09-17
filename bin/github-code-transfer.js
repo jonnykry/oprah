e76cda0dd81b1eff1462dc96d5bb5bb722ea4639
KrashLeviathan <nathankarasch@gmail.com>
2016-09-17 04:14:54 -0500

    Mostly-finished code transfer portion, with some stuff not yet working right.

--- bin/github-code-transfer.js
+++ bin/github-code-transfer.js
@@ -1,24 +1,59 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 
-var verbose = false;
+// Testing purposes only
+githubCodeTransfer("https://someurl", false);
 
 
-// Check if user has git installed
-if (!shell.which("git")) {
-    echo("Sorry, this script requires git.");
-    exit(1);
+function githubCodeTransfer(gforgeRepoURL, verbose) {
+    // Check if user has git installed
+    checkGitInstalled();
+    
+    // Begin script
+    console.log("Transferring Code from GitHub to GForge...");
+    shell.config.silent = !verbose;
+    // TODO: Uncomment below after complete
+    // attemptToCheckoutMaster();
+
+    // Tracks all remote branches
+    shell.exec("git branch -r | grep -v '\->' | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done");
+
+    // Pull down all branches
+    shell.exec("git fetch --all");
+    shell.exec("git pull --all");
+
+    // Setup gforge remote
+    setupGforgeRemote(gforgeRepoURL);
+
+    // Push to new remote
+    shell.exec("git push " + gforgeRepoURL + " --all");
+    shell.exec("git push " + gforgeRepoURL + " --tags");
 }
 
-// Log that the script is beginning...
-console.log("Transferring Code from GitHub to GForge...");
-shell.config.silent = !verbose;
+function checkGitInstalled() {
+    if (!shell.which("git")) {
+        shell.echo("Sorry, this script requires git.");
+        shell.exit(1);
+    }
+}
 
-// Tracks all remote branches
-// var gitBranch = shell.exec("git branch -r", {silent: !verbose}).stdout;
-// var grepResults = shell.grep("-v", "\->");
-shell.exec("git branch -r | grep -v '\->' | while read remote; do git branch --track \"${remote#origin/}\" \"$remote\"; done");
+function attemptToCheckoutMaster() {
+    var commandStdOut;
+    var commandStdErr;
+    shell.exec("git checkout master", function(_, stdout, stderr) {
+        commandStdOut = stdout;
+        commandStdErr = stderr;
+    });
+    if (commandStdErr) {
+        console.log(commandStdErr);
+        shell.exit(1);
+    }
+}
 
-// Pull down all branches
-shell.exec("git pull --all");
-
+function setupGforgeRemote(gforgeRepoURL) {
+    var remotes = shell.exec("git remote -v").stdout;
+    if (remotes.includes("gforge)")) {
+        shell.exec("git remote remove gforge");
+    }
+    shell.exec("git remote add gforge " + gforgeRepoURL);
+}
